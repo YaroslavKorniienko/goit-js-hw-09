bee91d390f07180b9d41180ab43115809f746777
YaroslavKorniienko <111692677+YaroslavKorniienko@users.noreply.github.com>
2023-02-16 17:38:37 +0000

    Deploying to gh-pages from @ YaroslavKorniienko/goit-js-hw-09@488948eb497ea13f3ea2bcbb469024da781a8c63 🚀

---
+++ 01-color-switcher.73af7a82.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;function d(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){a=setInterval(d,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.73af7a82.js.map
