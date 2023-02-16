488948eb497ea13f3ea2bcbb469024da781a8c63
YaroslavKorniienko <yarikkornej@gmail.com>
2023-02-16 19:36:47 +0200

    done all task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const bgColor = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(setBodyBgColor, 1000);
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+});
+
+stoptBtn.addEventListener('click', () => {
+  stoptBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBodyBgColor() {
+  return (bgColor.style.backgroundColor = getRandomHexColor());
+}
